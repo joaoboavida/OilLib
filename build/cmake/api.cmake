d54ffb852e0c4d117c3c072b50431b618ab72c58
biojppm <joao.magalhaes@biodroid-entertainment.com>
2012-01-23 15:33:35 +0000

    create cmake macro to make the api

--- build/cmake/api.cmake
+++ build/cmake/api.cmake
@@ -1,5 +1,9 @@
 # ######### Create the API  ##########
 
+find_package ( SWIG REQUIRED )
+include ( UseSWIG.cmake )
+include ( CMakeParseArguments )
+
 option ( BUILD_API_ADD_TO_ALL "Whether the API should be built by default" OFF )
 
 option ( BUILD_API_CSHARP "Whether to provide a target for generating the C# API" ON )
@@ -8,117 +12,130 @@ option ( BUILD_API_PYTHON "Whether to provide a target for generating the Python
 option ( BUILD_API_RUBY "Whether to provide a target for generating the Ruby API" ON )
 
 
-# many of the cmake find files were taken from gdcm, http://sourceforge.net/projects/gdcm/
+#===========================================
+
+#
+# SWIG_CREATE_API ( NAME name
+#                   OUTDIR dir
+#                   INTERFACE_FILES file1 file2 ...
+#                   SOURCE_FILES file1 file2 ...
+#                   ADDITIONAL_LIBS lib1 lib2
+#                 )
+#
+macro ( swig_create_api )
+
+  # ------------------------------------------------------------------
+  # parse arguments
+  #usage of cmake_parse_arguments: http://www.cmake.org/Wiki/CMakeMacroParseArguments
+  set ( options )
+  set ( oneValueArgs NAME OUTDIR )
+  set ( multiValueArgs INTERFACE_FILES SOURCE_FILES ADDITIONAL_LIBS )
+  cmake_parse_arguments ( SWGCA "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
+
+  #message ( STATUS "NAME=${SWGCA_NAME}" )
+  #message ( STATUS "OUTDIR=${SWGCA_OUTDIR}" )
+  #message ( STATUS "INTERFACE_FILES=${SWGCA_INTERFACE_FILES}" )
+  #message ( STATUS "SOURCE_FILES=${SWGCA_SOURCE_FILES}" )
+  #message ( STATUS "ADDITIONAL_LIBS=${SWGCA_ADDITIONAL_LIBS}" )
+
+  # General setup -------------------------
+  add_custom_target ( API COMMENT "Building API." )
+  include_directories ( "${SWGCA_OUTDIR}" )  # write .py, .php, etc in the outdir
+  # see http://www.cmake.org/Wiki/CMake_FAQ#How_do_I_use_CMake_to_generate_SWIG_wrapper_libraries.3F
+  set_source_files_properties ( ${SWGCA_INTERFACE_FILES} PROPERTIES CPLUSPLUS ON )
+  if ( API_ADD_TO_ALL )
+    add_dependencies ( all API )
+  endif ()
+
+  # many of the cmake find files were taken from gdcm:
+  # http://sourceforge.net/projects/gdcm/
+
+  # Csharp -------------------------
+  if ( BUILD_API_CSHARP )
+    find_package ( CSharp REQUIRED )
+    set ( apiName "api_csharp" )
+    set ( CMAKE_SWIG_OUTDIR "${SWGCA_OUTDIR}/csharp" )
+    file ( MAKE_DIRECTORY ${CMAKE_SWIG_OUTDIR} )
+    swig_add_module ( ${apiName} CSharp ${SWGCA_INTERFACE_FILES} ${SWGCA_SOURCE_FILES} )
+    # not necessary to add_incs_to_target()
+    set_target_properties ( ${apiName} PROPERTIES
+      EXCLUDE_FROM_ALL 1
+      ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
+      LIBRARY_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
+      RUNTIME_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
+      OUTPUT_NAME ${SWGCA_NAME}
+      COMMENT "API was put into ${CMAKE_SWIG_OUTDIR}"
+    )
+    target_link_libraries ( ${apiName} ${SWGCA_ADDITIONAL_LIBS} )
+    add_dependencies ( API ${apiName} )
+  endif ()
+
+  # PHP5 -------------------------
+  if ( BUILD_API_PHP5 )
+    find_package ( PHP5 REQUIRED )
+    set ( apiName "api_php5" )
+    set ( CMAKE_SWIG_OUTDIR "${SWGCA_OUTDIR}/php5" )
+    file ( MAKE_DIRECTORY ${CMAKE_SWIG_OUTDIR} )
+    swig_add_module ( ${apiName} php ${SWGCA_INTERFACE_FILES} ${SWGCA_SOURCE_FILES} )
+    add_incs_to_target ( ${apiName} ${PHP5_INCLUDE_PATH} ${CMAKE_SWIG_OUTDIR} )
+    set_target_properties ( ${apiName} PROPERTIES
+      EXCLUDE_FROM_ALL 1
+      ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
+      LIBRARY_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
+      RUNTIME_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
+      OUTPUT_NAME ${SWGCA_NAME}
+      COMMENT "API was put into ${CMAKE_SWIG_OUTDIR}"
+    )
+    target_link_libraries ( ${apiName} ${SWGCA_ADDITIONAL_LIBS} )
+    add_dependencies ( API ${apiName} )
+  endif ()
 
 
-# General setup -------------------------
-
-add_custom_target ( API COMMENT "Building API." )
-
-find_package ( SWIG REQUIRED )
-include ( UseSWIG.cmake )
-
-# write .py, .php, etc in the lib dir
-set ( OILLIB_SWIG_OUTDIR "${OILLIB_LIB_DIR}" )
-include_directories( "${OILLIB_LIB_DIR}" )
-
-# see http://www.cmake.org/Wiki/CMake_FAQ#How_do_I_use_CMake_to_generate_SWIG_wrapper_libraries.3F
-set_source_files_properties ( ${OILLIB_INTERFACE_FILES} PROPERTIES CPLUSPLUS ON )
-
-if ( API_ADD_TO_ALL )
-  add_dependencies ( all API )
-endif ()
-
-
-# Csharp -------------------------
-
-if ( BUILD_API_CSHARP )
-  find_package ( CSharp REQUIRED )
-  set ( apiName "api_csharp" )
-  set ( CMAKE_SWIG_OUTDIR "${OILLIB_SWIG_OUTDIR}/csharp" )
-  file ( MAKE_DIRECTORY ${CMAKE_SWIG_OUTDIR} )
-  swig_add_module ( ${apiName} CSharp ${OILLIB_INTERFACE_FILES} ${OILLIB_SOURCE_FILES} )
-  # not necessary to add_incs_to_target()
-  set_target_properties ( ${apiName} PROPERTIES
-    EXCLUDE_FROM_ALL 1
-    ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
-    LIBRARY_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
-    RUNTIME_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
-    OUTPUT_NAME oil
-    COMMENT "API was put into ${CMAKE_SWIG_OUTDIR}"
-  )
-  target_link_libraries ( ${apiName} OilLib )
-  add_dependencies ( API ${apiName} )
-endif ()
+  # Python -------------------------
+  if ( BUILD_API_PYTHON )
+    find_package ( PythonLibs REQUIRED )
+    set ( apiName "api_python" )
+    set ( CMAKE_SWIG_OUTDIR "${SWGCA_OUTDIR}/python" )
+    file ( MAKE_DIRECTORY ${CMAKE_SWIG_OUTDIR} )
+    swig_add_module ( ${apiName} python ${SWGCA_INTERFACE_FILES} ${SWGCA_SOURCE_FILES} )
+    swig_link_libraries ( ${apiName} ${PYTHON_LIBRARIES} )
+    set ( apiName "_${apiName}" )
+    add_incs_to_target ( ${apiName} "${PYTHON_INCLUDE_PATH}" )
+    set_target_properties ( ${apiName} PROPERTIES
+      EXCLUDE_FROM_ALL 1
+      ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
+      LIBRARY_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
+      RUNTIME_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
+      OUTPUT_NAME ${SWGCA_NAME}
+      PREFIX "_"  # name it as _extension.so (linux)
+      COMMENT "API was put into ${CMAKE_SWIG_OUTDIR}"
+    )
+    target_link_libraries ( ${apiName} ${SWGCA_ADDITIONAL_LIBS} )
+    add_dependencies ( API ${apiName} )
+  endif ()
 
 
-# PHP5 -------------------------
+  # Ruby -------------------------
+  if ( BUILD_API_RUBY )
+    find_package ( Ruby REQUIRED )
+    set ( apiName "api_ruby" )
+    set ( CMAKE_SWIG_OUTDIR "${SWGCA_OUTDIR}/ruby" )
+    file ( MAKE_DIRECTORY ${CMAKE_SWIG_OUTDIR} )
+    swig_add_module ( ${apiName} ruby ${SWGCA_INTERFACE_FILES} ${SWGCA_SOURCE_FILES} )
+    swig_link_libraries ( ${apiName} ${RUBY_LIBRARY} )
+    add_incs_to_target ( ${apiName} "${RUBY_INCLUDE_DIRS}" )
+    set_target_properties ( ${apiName} PROPERTIES
+      EXCLUDE_FROM_ALL 1
+      ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
+      LIBRARY_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
+      RUNTIME_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
+      OUTPUT_NAME ${SWGCA_NAME}
+      PREFIX "" # name it as extension.so (linux)
+      COMMENT "API was put into ${CMAKE_SWIG_OUTDIR}"
+    )
+    target_link_libraries ( ${apiName} ${SWGCA_ADDITIONAL_LIBS} )
+    add_dependencies ( API ${apiName} )
+  endif ()
 
-if ( BUILD_API_PHP5 )
-  find_package ( PHP5 REQUIRED )
-  set ( apiName "api_php5" )
-  set ( CMAKE_SWIG_OUTDIR "${OILLIB_SWIG_OUTDIR}/php5" )
-  file ( MAKE_DIRECTORY ${CMAKE_SWIG_OUTDIR} )
-  swig_add_module ( ${apiName} php ${OILLIB_INTERFACE_FILES} ${OILLIB_SOURCE_FILES} )
-  add_incs_to_target ( ${apiName} ${PHP5_INCLUDE_PATH} ${CMAKE_SWIG_OUTDIR} )
-  set_target_properties ( ${apiName} PROPERTIES
-    EXCLUDE_FROM_ALL 1
-    ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
-    LIBRARY_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
-    RUNTIME_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
-    OUTPUT_NAME oil
-    COMMENT "API was put into ${CMAKE_SWIG_OUTDIR}"
-  )
-  target_link_libraries ( ${apiName} OilLib )
-  add_dependencies ( API ${apiName} )
-endif ()
-
-
-# Python -------------------------
-
-if ( BUILD_API_PYTHON )
-  find_package ( PythonLibs REQUIRED )
-  set ( apiName "api_python" )
-  set ( CMAKE_SWIG_OUTDIR "${OILLIB_SWIG_OUTDIR}/python" )
-  file ( MAKE_DIRECTORY ${CMAKE_SWIG_OUTDIR} )
-  swig_add_module ( ${apiName} python ${OILLIB_INTERFACE_FILES} ${OILLIB_SOURCE_FILES} )
-  swig_link_libraries ( ${apiName} ${PYTHON_LIBRARIES} )
-  set ( apiName "_${apiName}" )
-  add_incs_to_target ( ${apiName} "${PYTHON_INCLUDE_PATH}" )
-  set_target_properties ( ${apiName} PROPERTIES
-    EXCLUDE_FROM_ALL 1
-    ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
-    LIBRARY_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
-    RUNTIME_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
-    OUTPUT_NAME oil
-    PREFIX "_"  # name it as _oil.so (linux)
-    COMMENT "API was put into ${CMAKE_SWIG_OUTDIR}"
-  )
-  target_link_libraries ( ${apiName} OilLib )
-  add_dependencies ( API ${apiName} )
-endif ()
-
-
-# Ruby -------------------------
-
-if ( BUILD_API_RUBY )
-  find_package ( Ruby REQUIRED )
-  set ( apiName "api_ruby" )
-  set ( CMAKE_SWIG_OUTDIR "${OILLIB_SWIG_OUTDIR}/ruby" )
-  file ( MAKE_DIRECTORY ${CMAKE_SWIG_OUTDIR} )
-  swig_add_module ( ${apiName} ruby ${OILLIB_INTERFACE_FILES} ${OILLIB_SOURCE_FILES} )
-  swig_link_libraries ( ${apiName} ${RUBY_LIBRARY} )
-  add_incs_to_target ( ${apiName} "${RUBY_INCLUDE_DIRS}" )
-  set_target_properties ( ${apiName} PROPERTIES
-    EXCLUDE_FROM_ALL 1
-    ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
-    LIBRARY_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
-    RUNTIME_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
-    OUTPUT_NAME oil
-    PREFIX "" # name it as oil.so (linux)
-    COMMENT "API was put into ${CMAKE_SWIG_OUTDIR}"
-  )
-  target_link_libraries ( ${apiName} OilLib )
-  add_dependencies ( API ${apiName} )
-endif ()
+endmacro ( swig_create_api )
 
