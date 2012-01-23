11114633d5e5b4922086ebf55bdb27f141252c1c
biojppm <joao.magalhaes@biodroid-entertainment.com>
2012-01-23 14:21:23 +0000

    added ruby API
    cmdline was:
    ( cd ~/OilLib/build/project
      &&
      ( rm -rf * ../../lib/* ;
        cmake -DBUILD_SHARED_LIBS:BOOL=ON -DBUILD_API:BOOL=ON ../cmake
        &&
        make all API
        &&
        ( cd ../../test/api/php5/ && php -d include_path='../../../lib/php5' -d enable_dl=On -d extension=../../../lib/php5/liboil.so -f runme.php && echo "---------OK pa" )
        &&
        ( cd ../../test/api/python && env PYTHONPATH=../../../lib/python python runme.py && echo "Prontos pa" )
        &&
        ( cd ../../test/api/csharp && dmcs -out:runme.exe ../../../lib/csharp/*.cs runme.cs && env LD_LIBRARY_PATH=../../../lib/csharp ./runme.exe && echo "------------------------Prontos pa" )
        &&
        ( cd ../../test/api/ruby && ruby -I../../../lib/ruby runme.rb && echo "------------------------Prontos pa" )
      )
    )

--- build/cmake/api.cmake
+++ build/cmake/api.cmake
@@ -1,11 +1,14 @@
 # ######### Create the API  ##########
 
-# many of the cmake find files were taken from gdcm, http://sourceforge.net/projects/gdcm/
-
 option ( BUILD_API_ADD_TO_ALL "Whether the API should be built by default" OFF )
+
 option ( BUILD_API_CSHARP "Whether to provide a target for generating the C# API" ON )
 option ( BUILD_API_PHP5 "Whether to provide a target for generating the PHP5 API" ON )
 option ( BUILD_API_PYTHON "Whether to provide a target for generating the Python API" ON )
+option ( BUILD_API_RUBY "Whether to provide a target for generating the Ruby API" ON )
+
+
+# many of the cmake find files were taken from gdcm, http://sourceforge.net/projects/gdcm/
 
 
 # General setup -------------------------
@@ -32,7 +35,7 @@ endif ()
 if ( BUILD_API_CSHARP )
   find_package ( CSharp REQUIRED )
   set ( apiName "api_csharp" )
-  set ( CMAKE_SWIG_OUTDIR "${OILLIB_SWIG_OUTDIR}/cs" )
+  set ( CMAKE_SWIG_OUTDIR "${OILLIB_SWIG_OUTDIR}/csharp" )
   file ( MAKE_DIRECTORY ${CMAKE_SWIG_OUTDIR} )
   swig_add_module ( ${apiName} CSharp ${OILLIB_INTERFACE_FILES} ${OILLIB_SOURCE_FILES} )
   # not necessary to add_incs_to_target()
@@ -42,6 +45,7 @@ if ( BUILD_API_CSHARP )
     LIBRARY_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
     RUNTIME_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
     OUTPUT_NAME oil
+    COMMENT "API was put into ${CMAKE_SWIG_OUTDIR}"
   )
   target_link_libraries ( ${apiName} OilLib )
   add_dependencies ( API ${apiName} )
@@ -63,6 +67,7 @@ if ( BUILD_API_PHP5 )
     LIBRARY_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
     RUNTIME_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
     OUTPUT_NAME oil
+    COMMENT "API was put into ${CMAKE_SWIG_OUTDIR}"
   )
   target_link_libraries ( ${apiName} OilLib )
   add_dependencies ( API ${apiName} )
@@ -86,6 +91,31 @@ if ( BUILD_API_PYTHON )
     LIBRARY_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
     RUNTIME_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
     OUTPUT_NAME _oil
+    COMMENT "API was put into ${CMAKE_SWIG_OUTDIR}"
+  )
+  target_link_libraries ( ${apiName} OilLib )
+  add_dependencies ( API ${apiName} )
+endif ()
+
+
+# Ruby -------------------------
+
+if ( BUILD_API_RUBY )
+  find_package ( Ruby REQUIRED )
+  set ( apiName "api_ruby" )
+  set ( CMAKE_SWIG_OUTDIR "${OILLIB_SWIG_OUTDIR}/ruby" )
+  file ( MAKE_DIRECTORY ${CMAKE_SWIG_OUTDIR} )
+  swig_add_module ( ${apiName} ruby ${OILLIB_INTERFACE_FILES} ${OILLIB_SOURCE_FILES} )
+  swig_link_libraries ( ${apiName} ${RUBY_LIBRARY} )
+  add_incs_to_target ( ${apiName} "${RUBY_INCLUDE_DIRS}" )
+  set_target_properties ( ${apiName} PROPERTIES
+    EXCLUDE_FROM_ALL 1
+    ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
+    LIBRARY_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
+    RUNTIME_OUTPUT_DIRECTORY "${CMAKE_SWIG_OUTDIR}"
+    OUTPUT_NAME oil
+    PREFIX ""             # the ruby extension is named extension.so, not libextension.so
+    COMMENT "API was put into ${CMAKE_SWIG_OUTDIR}"
   )
   target_link_libraries ( ${apiName} OilLib )
   add_dependencies ( API ${apiName} )
