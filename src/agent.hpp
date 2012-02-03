2f47e6fa38f02353d0673fea62efa22e6ab4b919
biojppm <joao.magalhaes@biodroid-entertainment.com>
2012-02-03 18:40:32 +0000

    add a market class

--- src/agent.hpp
+++ src/agent.hpp
@@ -8,6 +8,8 @@
 
 namespace oil {
 
+template< typename Type > class market;
+
 template< typename Type >
 class agent
 {
@@ -16,10 +18,12 @@ public:
   typedef Type value_type;
   typedef time_series< Type > series_type;
   typedef trading_rule< Type > rule_type;
+  typedef market< Type > market_type;
 
-  agent () {}
+  agent () : m_market(0) {}
   agent ( value_type cash )
    :
+     m_market ( 0 ),
      m_initialCash ( cash ),
      m_currentCash ( cash ),
      m_positions ( 0 )
@@ -27,16 +31,15 @@ public:
   }
   ~agent () {}
 
-  void init ( value_type cash )
+  void init ( value_type cash, market_type &m )
   {
     m_initialCash = cash;
     m_currentCash = cash;
     m_positions = 0;
+    m_market = &m;
   }
 
-  void setOilPrice ( series_type &s ) { m_oilPrice = &s; }
   void setTradingRule ( rule_type *r ) { m_rule = r; }
-  static void setContractSize ( const value_type size ) { m_contractSize = size; }
 
   value_type getCash() { return m_currentCash; }
   void setCash ( value_type c ) { m_currentCash = c; }
@@ -48,7 +51,7 @@ public:
 
   typename rule_type::BuySignal decide()
   {
-    return (*m_rule)( *m_oilPrice );
+    return (*m_rule)( m_market->getOilPrice() );
   }
 
   int computeDemand ()
@@ -62,7 +65,7 @@ public:
     // compute wanted position - absolute
     m_positions = ( sigval * m_currentCash )
                   /
-                  ( m_contractSize * m_oilPrice->getVolatility() );
+                  ( m_market->getContractSize() * m_market->getOilPrice().getVolatility() );
 
     return m_positions;
   }
@@ -71,7 +74,7 @@ public:
   {
     value_type PnL = nFinalPositions
                  *
-                 m_oilPrice->getReturn() * m_contractSize; // unit return
+                 m_market->getOilPrice().getReturn() * m_market->getContractSize(); // unit return
     m_currentCash += PnL;
     m_positions = 0;
     return PnL;
@@ -79,20 +82,15 @@ public:
 
 private:
 
-  series_type *m_oilPrice;
+  market_type *m_market;
   rule_type *m_rule;
 
   value_type m_initialCash;
   value_type m_currentCash;
 
-  static int m_contractSize;
-
   int m_positions;
 
 };
-
-template< typename Type >
-int agent< Type >::m_contractSize = 1000;
 
 } // namespace oil
 
--- test/000/test.cpp
+++ test/000/test.cpp
@@ -1,8 +1,9 @@
 
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
 
-#include "agent.hpp"
+#include "market.hpp"
 
 using namespace oil;
 
@@ -11,67 +12,52 @@ typedef trading_rule< double > TradingRule;
 typedef moving_average_crossover< double > TradingRuleAVG;
 typedef lag_crossover< double > TradingRuleLAG;
 typedef agent< double > Agent;
-
-
-template< typename Type >
-class market
-{
-public:
-  typedef Type value_type;
-  typedef agent< value_type > agent_type;
-
-  typedef std::vector< agent_type* > container_type;
-
-  typedef typename container_type::iterator iterator;
-  typedef typename container_type::const_iterator const_iterator;
-  typedef typename container_type::reverse_iterator reverse_iterator;
-  typedef typename container_type::const_reverse_iterator const_reverse_iterator;
-
-  market ( unsigned int n ) : m_agents(n) {}
-
-  iterator begin () { return m_agents.begin(); }
-  const_iterator begin () const { return m_agents.begin(); }
-  iterator end () { return m_agents.end(); }
-  const_iterator end () const { return m_agents.end(); }
-
-  reverse_iterator rbegin () { return m_agents.rbegin(); }
-  const_reverse_iterator rbegin () const { return m_agents.rbegin(); }
-  reverse_iterator rend () { return m_agents.rend(); }
-  const_reverse_iterator rend () const { return m_agents.rend(); }
-
-  const unsigned int size() const { return m_agents.size(); }
-
-  value_type& back() { return m_agents.back().value; }
-  const value_type& back () const { return m_agents.back().value; }
-
-private:
-  std::vector< agent_type* > m_agents;
-};
-
 typedef market< double > Market;
-
-
-void blabla( Market& a )
-{
-  int positions = a.computeDemand();
-  int trade = positions;
-//  a.setCash ( 10000 );
-  double PnL = a.trade( trade );
-//  a.setCash ( 10000 );
-
-  std::cout << " posn=" << positions
-            << " trade=" << trade
-            << " PnL=" << PnL
-            << " pcPnL=" << PnL / a.getWealth() * 100.0
-            << " cash=" << a.getCash()
-            << std::endl;
-}
-
 
 int main(int argc, char *argv[])
 {
+  Market myMarket ( 10 );
 
-  TimeSeries s;
+  myMarket.setContractSize(1000);
+
+  TradingRuleLAG ruleLag(2, 8);
+  TradingRuleAVG ruleAvg(2, 8);
+
+  for ( Market::iterator it = myMarket.begin(); it != myMarket.end(); ++it)
+  {
+    Agent& a = *(*it);
+
+    double initialCash = 1000;
+    a.init(initialCash, myMarket);
+
+    double r = (double) rand() / (double) RAND_MAX;
+    if ( r < 0.5 ) a.setTradingRule( &ruleLag );
+    else a.setTradingRule( &ruleAvg );
+  }
+
+  std::ifstream prices("prices.dat");
+  std::string sdate, sprice;
+  prices >> sdate >> sprice;
+  std::cout << sdate << "  " << sprice << std::endl;
+
+  int timeLevel = 0;
+  for(;;)
+  {
+    double v;
+    prices >> sdate >> v;
+    if(prices.good()) myMarket.getOilPrice().push_back(v);
+    else break;
+
+    std::cout << "timeLevel=" << timeLevel << " " << std::endl;
+    myMarket.trade();
+
+    timeLevel++;
+    if ( timeLevel > 20 ) break;
+  }
+
+
+
+//  TimeSeries s;
 /*
   for(int i = 0; i < 100; ++i)
   {
@@ -90,7 +76,7 @@ int main(int argc, char *argv[])
   }
   return 0;
 */
-
+/*
   std::ifstream prices("prices.dat");
   std::string sdate, sprice;
   prices >> sdate >> sprice;
@@ -176,8 +162,12 @@ int main(int argc, char *argv[])
     oldPositions = positions;
     oldSewma = sewma;
     oldLewma = lewma;
-  }
 
+    myMarket.trade();
+  }
+*/
+
+  prices.close();
   return 0;
 }
 
