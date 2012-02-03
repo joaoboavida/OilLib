1ab9358e60b5edbfcaea9942e9270be0f0e1e950
biojppm <joao.magalhaes@biodroid-entertainment.com>
2012-02-03 20:29:46 +0000

    implemented a pro-rata auction

--- src/market.hpp
+++ src/market.hpp
@@ -24,7 +24,7 @@ public:
   typedef typename container_type::reverse_iterator reverse_iterator;
   typedef typename container_type::const_reverse_iterator const_reverse_iterator;
 
-  market ( unsigned int n ) : m_agents(n)
+  market ( unsigned int n ) : m_agents(n), m_alpha(0.01)
   {
     for( unsigned i = 0; i < n; ++i )
     {
@@ -62,13 +62,58 @@ public:
   series_type& getOilPrice () { return m_oilPrice; }
   const series_type& getOilPrice () const { return m_oilPrice; }
 
-  void trade ()
+  /** @return missing positions */
+  int trade ()
   {
 
     std::cout << "MARKET: price=" << m_oilPrice.current()
               << " return=" << m_oilPrice.getReturn()
               << " vol=" << m_oilPrice.getVolatility()
               << " adj=" << m_oilPrice.getAdjustedReturn()
+              << std::endl;
+
+    int demand = 0, supply = 0;
+    for (
+          typename container_type::iterator it = m_agents.begin();
+          it != m_agents.end();
+          ++it
+        )
+    {
+      agent_type& a = *(*it);
+      int positions = a.computeDemand();
+std::cout << std::endl;
+
+      if(positions > 0) demand += positions;
+      else if(positions < 0) supply -= positions;
+    }
+
+    int missing = demand - supply;
+    double ratioDemand, ratioSupply;
+
+
+    if ( demand > supply )
+    {
+      ratioDemand = value_type(supply) / value_type(demand);
+      ratioSupply = value_type(1);
+    }
+    else if ( demand == supply )
+    {
+      ratioDemand = value_type(1);
+      ratioSupply = value_type(1);
+    }
+    else
+    {
+      ratioDemand = value_type(1);
+      ratioSupply = value_type(demand) / value_type(supply);
+    }
+
+    std::cout << "MARKET: demand=" << demand
+              << " supply=" << supply
+              << " missing=" << missing
+              << " ratioDemand=" << ratioDemand
+              << " ratioSupply=" << ratioSupply
+              << " OLD_PRICE=" << m_oilPrice.current()
+              << " NEW PRICE=" << computeNewOilPrice( missing )
               << std::endl;
 
     unsigned int i = 0;
@@ -80,8 +125,11 @@ public:
     {
       std::cout << "AGENT[" << i << "]: ";
       agent_type& a = *(*it);
-      int positions = a.computeDemand();
+
+      int positions = a.getDemand();
       int trade = positions;
+      trade *= ( positions >= 0 ) ? ratioDemand : ratioSupply;
+
     //  a.setCash ( 10000 );
       double PnL = a.trade( trade );
     //  a.setCash ( 10000 );
@@ -95,11 +143,22 @@ public:
     }
     std::cout << std::endl;
     std::cout << std::endl;
+
+    return missing;
+  }
+
+  value_type computeNewOilPrice( int missingPositions )
+  {
+    return m_oilPrice.current() + m_alpha * missingPositions;
   }
 
 private:
 
   std::vector< agent_type* > m_agents;
+
+  /** oil price elasticity */
+  value_type m_alpha;
+
   series_type m_oilPrice;
 
   static int m_contractSize;
--- test/000/test.cpp
+++ test/000/test.cpp
@@ -14,14 +14,22 @@ typedef lag_crossover< double > TradingRuleLAG;
 typedef agent< double > Agent;
 typedef market< double > Market;
 
+
+typedef std::vector< TradingRule * > ExistingRules;
+
 int main(int argc, char *argv[])
 {
-  Market myMarket ( 10 );
+  Market myMarket ( 100 );
 
   myMarket.setContractSize(1000);
 
-  TradingRuleLAG ruleLag(2, 8);
-  TradingRuleAVG ruleAvg(2, 8);
+  ExistingRules rules;
+  for ( int i = 0; i < 8; ++i )
+  {
+    int s = pow(2, i+1);
+    rules.push_back ( new TradingRuleLAG( s, s*2 ) );
+    rules.push_back ( new TradingRuleAVG( s, s*2 ) );
+  }
 
   for ( Market::iterator it = myMarket.begin(); it != myMarket.end(); ++it)
   {
@@ -30,9 +38,9 @@ int main(int argc, char *argv[])
     double initialCash = 1000;
     a.init(initialCash, myMarket);
 
-    double r = (double) rand() / (double) RAND_MAX;
-    if ( r < 0.5 ) a.setTradingRule( &ruleLag );
-    else a.setTradingRule( &ruleAvg );
+    int pos = rules.size() * ((double) rand() / (double) RAND_MAX);
+    a.setTradingRule( rules[pos] );
+
   }
 
   std::ifstream prices("prices.dat");
@@ -55,119 +63,12 @@ int main(int argc, char *argv[])
     if ( timeLevel > 20 ) break;
   }
 
-
-
-//  TimeSeries s;
-/*
-  for(int i = 0; i < 100; ++i)
+  // clean the trading rules
+  for ( ExistingRules::iterator it = rules.begin(); it != rules.end(); ++it )
   {
-    s.push_back(i + 1);
-    std::cout << "i=" << i << ": size=" << s.size()
-              << "  last=" << s.back()
-              << "     av1=" << s.getMovingAverage( 1 )
-              << " .v1=" << s.getMovingAverage( 1, --s.end() )
-              << "  av2=" << s.getMovingAverage( 2 )
-              << " .v2=" << s.getMovingAverage( 2, --s.end() )
-              << "  av4=" << s.getMovingAverage( 4 )
-              << " .v4=" << s.getMovingAverage( 4, --s.end() )
-              << "  av8=" << s.getMovingAverage( 8 )
-//              << " .v8=" << s.getMovingAverage( 8, --s.end() )
-              << std::endl;
+    delete *it;
   }
-  return 0;
-*/
-/*
-  std::ifstream prices("prices.dat");
-  std::string sdate, sprice;
-  prices >> sdate >> sprice;
-  std::cout << sdate << "  " << sprice << std::endl;
 
-  double fundSize = 10000,
-         contractSize = 1000;
-
-  TradingRuleLAG ruleLag(2, 8);
-  TradingRuleAVG ruleAvg(2, 8);
-
-  Agent firstAgent, secondAgent;
-
-  Agent::setContractSize(1000);
-
-  firstAgent.setTradingRule(&ruleLag);
-  firstAgent.setOilPrice(s);
-  firstAgent.init(fundSize);
-
-  secondAgent = firstAgent;
-  secondAgent.setTradingRule(&ruleAvg);
-
-  int i = 0;
-  int oldPositions = 0;
-  double oldSewma = -1.18,
-         oldLewma = -1.18;
-  for(;;)
-  {
-    double v;
-    prices >> sdate >> v;
-    if(prices.good()) s.push_back(v);
-    else break;
-
-    TimeSeries::iterator it = s.end();
-    --it;
-    double price = it->value,
-           ret = s.getReturn(it),
-           vol = s.getVolatility(it),
-           adj = s.getAdjustedReturn(it),
-           ws = double(1) / double(2),
-           sewma = (1-ws)*oldSewma + ws*price, //
-        // sewma = s.getMovingAverage(2),
-           wl = double(1) / double(8),
-           lewma = (1-wl)*oldLewma + wl*price, //
-        // lewma = s.getMovingAverage(8),
-           crossover = sewma - lewma;
-
-    int signal;
-    if( crossover > 0 ) signal = 1;
-    else if( crossover < 0 ) signal = -1;
-    else signal = 0;
-
-    int positions = fundSize * signal / ( contractSize * vol ),
-        trade = positions - oldPositions;
-
-    double PnL = oldPositions * ret * contractSize;
-
-    std::cout << "MARKET: price[" << i << "]=" << price
-              << " return=" << ret
-              << " vol=" << vol
-              << " adj=" << adj
-              << std::endl;
-
-    std::cout << "EXCELOLD:"
-              << " sewma=" << sewma
-              << " lewma=" << lewma
-              << " crossover=" << crossover
-              << " signal=" << signal
-              << " posn=" << positions
-              << " trade=" << trade
-              << " PnL=" << PnL
-              << " pcPnL=" << PnL/fundSize * 100.0
-              << " cash=" << fundSize
-              << std::endl;
-
-    std::cout << "AGENTLAG: ";
-    blabla ( firstAgent );
-    std::cout << "AGENTAVG: ";
-    blabla ( secondAgent );
-    std::cout << std::endl;
-
-    i++;
-    oldPositions = positions;
-    oldSewma = sewma;
-    oldLewma = lewma;
-
-    myMarket.trade();
-  }
-*/
-
-  prices.close();
   return 0;
 }
 
